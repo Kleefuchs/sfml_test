31613ff7d5279de128a8f3626a9ce4491d1baabf
Kleefuchs <sam.brecht@outlook.de>
2025-09-28 13:36:33 +0200

    Fixed stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,23 @@
 
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <optional>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({1600, 900}),
+                            "Boykisser Game UwU :3");
+    const sf::Texture playerTexture("assets/player.png");
+    sf::Sprite player(playerTexture);
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+        window.clear();
+        window.draw(player);
+        window.display();
+    }
+}
