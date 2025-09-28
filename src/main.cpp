95091e49aeb0c8de89d63a3a6b85b04399500f0c
Kleefuchs <sam.brecht@outlook.de>
2025-09-28 11:03:45 +0200

    game.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,1 @@
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Window/VideoMode.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode({1600, 900}), "Boykissies");
-    while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent()) {
-            // Close window: exit
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-        window.clear();
-        window.display();
-    }
-}
